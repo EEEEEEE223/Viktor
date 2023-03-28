9df9c2bf35e35f3846ea816a0538c4124683cd7d
саша <lastik338@gmail.com>
2023-03-28 13:39:45 +0300

    ddd

---
+++ app/src/main/java/com/example/viktor/DrawThread.java
@@ -0,0 +1,64 @@
+package com.example.viktor;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+
+public class DrawThread extends Thread {
+
+    private SurfaceHolder surfaceHolder;
+
+    private volatile boolean running = true; // флаг для остановки потока
+    private Paint backgroundPaint = new Paint();
+    private Paint pain = new Paint();
+    private Bitmap bitmap;
+    private int towardPointX;
+    private int towardPointY;
+
+    {
+        pain.setColor(R.drawable.b1);
+        backgroundPaint.setColor(Color.BLUE);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public DrawThread(Context context, SurfaceHolder surfaceHolder) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.smile);
+        this.surfaceHolder = surfaceHolder;
+    }
+
+    public void requestStop() {
+        running = false;
+    }
+
+    public void setTowardPoint(int x, int y) {
+        towardPointX = x;
+        towardPointY = y;
+    }
+
+    @Override
+    public void run() {
+        int smileX = 0;
+        int smileY = 0;
+        while (running) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+            if (canvas != null) {
+                try {
+                    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+                    canvas.drawRect(canvas.getWidth()-100, canvas.getHeight()-200, canvas.getWidth(), canvas.getHeight(), pain);
+                    canvas.drawBitmap(bitmap, smileX, smileY, backgroundPaint);
+                    if (smileX + bitmap.getWidth() / 2 < towardPointX) smileX += 10;
+                    if (smileX + bitmap.getWidth() / 2 > towardPointX) smileX -= 10;
+                    if (smileY + bitmap.getHeight() / 2 < towardPointY) smileY += 10;
+                    if (smileY + bitmap.getHeight() / 2 > towardPointY) smileY -= 10;
+                } finally {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+
+            }
+        }
+    }
+}
