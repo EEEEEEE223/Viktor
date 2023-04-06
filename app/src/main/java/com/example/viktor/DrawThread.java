49e2ec9b959c9757ed5f5f01d78d15c38ac81010
Viktor <lastik338@gmail.com>
2023-04-06 18:06:10 +0300

    d

--- app/src/main/java/com/example/viktor/DrawThread.java
+++ app/src/main/java/com/example/viktor/DrawThread.java
@@ -12,13 +12,14 @@ import android.view.SurfaceHolder;
 public class DrawThread extends Thread {
 
     private SurfaceHolder surfaceHolder;
-
     private volatile boolean running = true;
     private Paint backgroundPaint = new Paint();
     private Bitmap bitmap;
-    private Bitmap b1;
-    private Bitmap b2;
+    private Bitmap but;
     private MyButton up;
+    private MyButton down;
+    private MyButton left;
+    private MyButton right;
     private int towardPointX;
     private int towardPointY;
     {
@@ -29,8 +30,7 @@ public class DrawThread extends Thread {
     public DrawThread(Context context, SurfaceHolder surfaceHolder) {
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.smile);
         this.surfaceHolder = surfaceHolder;
-        b1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.b1);
-        b2 = BitmapFactory.decodeResource(context.getResources(), R.drawable.b2);
+        but = BitmapFactory.decodeResource(context.getResources(), R.drawable.but);
     }
 
     public void requestStop() {
@@ -48,25 +48,44 @@ public class DrawThread extends Thread {
         int smileY = 0;
         while (running) {
             Canvas canvas = surfaceHolder.lockCanvas();
-            up=new MyButton(150,500,b1,canvas);
+            up=new MyButton(200,500,but,canvas);
+            down=new MyButton(200,500,but,canvas);
+            right=new MyButton(400,350,but,canvas);
+            left=new MyButton(0,350,but,canvas);
+
             if (canvas != null) {
                 try {
                     canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+                    canvas.save();
+                    canvas.scale(0.5F,0.5F);
                     canvas.drawBitmap(bitmap, smileX, smileY, backgroundPaint);
-                    Rect src = new Rect(0, 0, up.getBitmap().getWidth(),up.getBitmap().getHeight());
+                    canvas.restore();
+                    Rect src = new Rect( up.getBitmap().getWidth()/2,up.getBitmap().getHeight()/2,up.getBitmap().getWidth(), up.getBitmap().getHeight());
+                    Rect src2 = new Rect( 0,down.getBitmap().getHeight()/2,down.getBitmap().getWidth()/2, down.getBitmap().getHeight());
+                    Rect src3 = new Rect( 0,0,left.getBitmap().getWidth()/2, left.getBitmap().getHeight()/2);
+                    Rect src4 = new Rect( right.getBitmap().getWidth()/2,0,right.getBitmap().getWidth(), right.getBitmap().getHeight()/2);
                     Rect destination = new Rect(up.getX(), up.getY2(), up.getX()+200,up.getY2()+200);
-
+                    Rect destination2 = new Rect(down.getX(), canvas.getHeight()-200 ,down.getX()+200,canvas.getHeight());
+                    Rect destination3 = new Rect(left.getX(), left.getY2(), left.getX()+200,left.getY2()+200);
+                    Rect destination4 = new Rect(right.getX(), right.getY2(), right.getX()+200,right.getY2()+200);
                     canvas.drawBitmap(up.getBitmap(),src,destination,new Paint());
-                    if (smileX + bitmap.getWidth() / 2 < towardPointX) smileX += 10;
-                    if (smileX + bitmap.getWidth() / 2 > towardPointX) smileX -= 10;
-                    if (smileY+bitmap.getHeight()/2<towardPointY) {
+                    canvas.drawBitmap(down.getBitmap(),src2,destination2,new Paint());
+                    canvas.drawBitmap(left.getBitmap(),src3,destination3,new Paint());
+                    canvas.drawBitmap(right.getBitmap(),src4,destination4,new Paint());
+                    if (destination4.contains(towardPointX,towardPointY)) {
+                        if(canvas.getWidth()-smileX<=smileX)smileX=canvas.getWidth()-canvas.getWidth();
+                        smileX += 10;
+                    }
+                    if (destination3.contains(towardPointX,towardPointY)){
+                        if(smileX<=0) smileX=0;
+                        smileX -= 10;
+                    }
+                    if (destination2.contains(towardPointX,towardPointY)){
+                        if (smileY+bitmap.getHeight()+25>=up.getY2())smileY=up.getY2()-bitmap.getHeight()-25;
                         smileY += 10;
                     }
                     if (destination.contains(towardPointX,towardPointY)) {
-                        if (smileY+bitmap.getHeight()>=up.getY2())smileY=up.getY2()-bitmap.getHeight();
-                        System.out.println(smileY);
-                        System.out.println(towardPointX + "hj");
-                        System.out.println(towardPointY);
+                        if(smileY<=0) smileY=0;
                         smileY -= 10;
                     }
                 } finally {
---
+++ app/src/main/java/com/example/viktor/MyLevel.java
@@ -0,0 +1,67 @@
+package com.example.viktor;
+
+import android.os.CountDownTimer;
+
+public class MyLevel {
+    int level;
+    int seconds;
+    int time=seconds;
+    int enemy;
+    int strongenemy;
+    int longrangeenemy;
+    CountDownTimer upCountDownTimer=new CountDownTimer(seconds,1000) {
+        @Override
+        public void onTick(long l) {
+            time--;
+        }
+
+        @Override
+        public void onFinish(){
+
+        }
+    };
+
+    public MyLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getEnemy() {
+        return enemy;
+    }
+
+    public void setEnemy(int enemy) {
+        this.enemy = enemy;
+    }
+
+    public int getStrongenemy() {
+        return strongenemy;
+    }
+
+    public void setStrongenemy(int strongenemy) {
+        this.strongenemy = strongenemy;
+    }
+
+    public int getLongrangeenemy() {
+        return longrangeenemy;
+    }
+
+    public void setLongrangeenemy(int longrangeenemy) {
+        this.longrangeenemy = longrangeenemy;
+    }
+}
